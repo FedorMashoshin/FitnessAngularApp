7f4f7e263381093251e051ba673534c778f1939d
Fedor_Mashoshin <fmashoshin@gmail.com>
2021-10-25 14:07:41 -0700

    Add few components

--- FitnessAngularApp/src/app/app.module.ts
+++ FitnessAngularApp/src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterailModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentComponent } from './training/current/current.component';
+import { NewComponent } from './training/new/new.component';
+import { PreviousComponent } from './training/previous/previous.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentComponent,
+    NewComponent,
+    PreviousComponent
   ],
   imports: [
     BrowserModule,
