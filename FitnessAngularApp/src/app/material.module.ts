67681f8f755990604be8af89f91c5490fb380a75
Fedor_Mashoshin <fmashoshin@gmail.com>
2021-11-22 13:29:07 -0800

    Add a datepicker for signup page

--- FitnessAngularApp/src/app/material.module.ts
+++ FitnessAngularApp/src/app/material.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
+
 
 
 
 @NgModule({
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatFormFieldModule, MatInputModule]
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ]
 })
 export class MaterailModule{}
