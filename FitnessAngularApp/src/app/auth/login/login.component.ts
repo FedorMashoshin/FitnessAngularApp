198f5a2916e425895141c6e4b01a874cee85cd22
Fedor_Mashoshin <fmashoshin@gmail.com>
2021-12-07 12:11:15 -0800

    Create login page

--- FitnessAngularApp/src/app/auth/login/login.component.ts
+++ FitnessAngularApp/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  password: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    console.log('form:: ', form)
   }
 
 }
